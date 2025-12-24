b9e4963b6a77bf990c636494a4447e5593898806
Rizka Nur Octaviani <Rizka Nur Octaviani@LAPTOP-PAS2NDAS>
2025-12-24 09:35:38 +0700

    Setup DbContext & koneksi database

---
+++ SistemAntrianAlat/DAL/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using SistemAntrianAlat.Models;
+
+namespace SistemAntrianAlat.DAL
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext() : base("name=ApplicationContext")
+        {
+
+        }
+        public DbSet<Pengajuan> Pengajuan { get; set; }
+        //public DbSet<SoilTest> SoilTest { get; set; }
+    }
+}
---
+++ SistemAntrianAlat/Models/Pengajuan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemAntrianAlat.Models
+{
+    public class Pengajuan
+    {
+        public int PengajuanId { get; set; }
+        public string Alat { get; set; }
+        public string Alasan { get; set; }
+        public string Status { get; set; }
+        public DateTime TglPengajuan { get; set; }
+
+    }
+}
